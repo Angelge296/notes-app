1f1080addd9a407cd6996074e60f442ed0e4b945
Ángel Gómez Espinosa <angelge296@gmail.com>
2021-08-26 20:16:21 +0200

    Folder structure

---
+++ node-app/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+//Initializations
+const app = express();
+
+//Setting
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+//Middlewares
+
+// Global Variables
+
+// Routes
+
+// Static Files
+
+// Server is listenning
+
+app.listen(app.get('port'), ()=>{
+    console.log('Server on port ',app.get('port'));
+})
