52a0cedae5940d6198b2eeaf590785d40354e509
Ángel Gómez Espinosa <angelge296@gmail.com>
2021-08-27 20:18:55 +0200

    Connection with DB and creating HBS files

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
 
 //Initializations
 const app = express();
-
+require('./database');
 //Setting
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -16,13 +19,21 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', 'hbs');
 //Middlewares
-
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'mysecrectapp', 
+    resave: true,
+    saveUninitialized: true
+}));
 // Global Variables
 
 // Routes
-
+app.use(require('./routes/index'));
+app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
 // Static Files
-
+app.use(express.static(path.join(__dirname, "public")));
 // Server is listenning
 
 app.listen(app.get('port'), ()=>{
